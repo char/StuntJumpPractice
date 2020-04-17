2e4d8d192bd58e86a1f69c02f27b4245429deaec
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-04-17 05:52:18 +0100

    Oops! Actually register the keyboard handler

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "../../inc/main.h"
+#include "keyboard.h"
 #include "script.h"
 
 BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved)
@@ -13,9 +14,11 @@ BOOL APIENTRY DllMain(HMODULE hInstance, DWORD reason, LPVOID lpReserved)
 	{
 	case DLL_PROCESS_ATTACH:
 		scriptRegister(hInstance, ScriptMain);
+		keyboardHandlerRegister(OnKeyboardMessage);
 		break;
 	case DLL_PROCESS_DETACH:
 		scriptUnregister(hInstance);
+		keyboardHandlerUnregister(OnKeyboardMessage);
 		break;
 	}		
 	return TRUE;
